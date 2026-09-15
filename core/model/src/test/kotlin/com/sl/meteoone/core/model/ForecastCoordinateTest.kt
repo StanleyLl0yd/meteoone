28efaa6bb83afaa7dc228948584192c3d632b339
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-15 20:56:11 +0300

    test: reject floating-point forecast identity aliases

--- core/model/src/test/kotlin/com/sl/meteoone/core/model/ForecastCoordinateTest.kt
+++ core/model/src/test/kotlin/com/sl/meteoone/core/model/ForecastCoordinateTest.kt
@@ -5,7 +5,7 @@ import kotlin.test.assertFailsWith
 
 class ForecastCoordinateTest {
     @Test
-    fun rejectsInvalidOrNonNormalizedCoordinates() {
+    fun rejectsInvalidNonCanonicalOrNonNormalizedCoordinates() {
         listOf(
             Double.NaN to 0.0,
             Double.POSITIVE_INFINITY to 0.0,
@@ -13,6 +13,10 @@ class ForecastCoordinateTest {
             0.0 to Double.NaN,
             0.0 to 180.0,
             0.0 to 180.1,
+            -0.0 to 0.0,
+            0.0 to -0.0,
+            (0.1 + 0.2) to 56.8,
+            59.900000000000006 to 30.3,
             12.34 to 56.8,
             12.3 to 56.78,
         ).forEach { (latitude, longitude) ->
