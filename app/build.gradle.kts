387d53a721843d7a57f3fb32b257fb809edffa47
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-17 13:05:47 +0300

    M3: enable app unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,5 +104,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.kotlinx.coroutines.core)
 
+    testImplementation(libs.junit)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
