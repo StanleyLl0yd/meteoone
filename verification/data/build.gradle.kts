a8e55bc9beab5cec5cff2d7c2bb186d54fcaf848
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-18 18:10:49 +0300

    M4: add verification data module

---
+++ verification/data/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    api(project(":verification:domain"))
+    implementation(project(":core:network"))
+
+    testImplementation(libs.kotlin.test.junit)
+    testImplementation(libs.junit)
+}
